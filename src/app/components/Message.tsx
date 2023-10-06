dffb657b9e4d9675adf56c8e1f80429d721cd18e
Tam Nguyen <nguyenminhtam7124@gmail.com>
2023-10-06 18:27:12 -0400

    Update chat page

--- src/app/components/Message.tsx
+++ src/app/components/Message.tsx
@@ -1,11 +1,39 @@
-// Message.tsx
+"use client";
+
+import React, { useState } from "react";
 
 interface Props {
   message: string;
   sender: string | null;
 }
 
-export function MessageBubble({ message, sender }: Props) {
+export function MessagesList() {
+  // load conversation history from api
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello world",
+      sender: "Me",
+    },
+    {
+      message: "How are you?",
+      sender: null,
+    },
+  ]);
+
+  return (
+    <div className="chatroom flex flex-col items-center justify-start w-full h-[80vh] overflow-y-auto">
+      {messages.map((message, index) => (
+        <MessageBubble
+          key={index}
+          message={message.message}
+          sender={message.sender}
+        />
+      ))}
+    </div>
+  );
+}
+
+function MessageBubble({ message, sender }: Props) {
   const messageContainerClass =
     "message-container flex flex-row items-center justify-center max-w-1/3 max-h-1/3 overflow-hidden m-2 p-2 " +
     (sender ? "ml-auto" : "mr-auto");
@@ -30,13 +58,29 @@ export function MessageBubble({ message, sender }: Props) {
   );
 }
 
-export function MessageInput () {
+export function MessageInput() {
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    // only send when hit enter
+    if (event.key !== "Enter") return;
+
+    event.preventDefault();
+
+    // add the message to the messages list
+    // by sending new messsage to the api
+
+    setMessage("");
+  };
+
   return (
-    <div className="message-input flex flex-row items-center justify-center w-full bg-gray-200 border-t border-gray-300">
-      <input
-        type="text"
-        className="message-input-field w-full h-full p-2 text-sm text-gray-700 outline-none"
+    <div className="message-input flex flex-row items-center justify-center self-end w-full h-full border-t border-gray-800 h-[13vh]">
+      <textarea
+        className="message-input-field flex-1 w-full h-full p-2 m-auto text-sm text-gray-700 outline-none resize-none"
         placeholder="Type a message..."
+        value={message}
+        onKeyDown={sendMessage}
+        onChange={(event) => setMessage(event.target.value)}
       />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
-import { MessageBubble, MessageInput } from "./components/Message";
+import { MessagesList, MessageInput } from "./components/Message";
+import { Header } from "./components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0 bg-white">
-      <div className="main-div z-10 max-w-7xl w-full max-h-screen items-center justify-between font-mono text-sm lg:flex flex-row h-full">
+      <div className="main-div z-10 max-w-7xl w-full max-h-screen items-center justify-between font-mono text-sm lg:flex flex-row border border-red-800">
         <div className="left-col hidden flex-col items-center justify-center lg:flex w-1/3 bg-gray-700">
-          <div className="content flex flex-col items-center justify-center w-full h-screen bg-gray-700">
+          <div className="content flex flex-col items-center justify-center w-full">
             Nothing here yet
           </div>
         </div>
-        <div className="right-col flex flex-col items-center justify-center w-full bg-white h-full self-start">
+        <div className="right-col flex flex-col items-center justify-start w-full bg-white h-full min-h-screen self-start border border-gray-800">
           <div className="header flex flex-row items-center justify-between w-full bg-gray-200 border-b border-gray-300">
-            Header
+            <Header />
           </div>
 
           <div className="body flex flex-col items-center justify-center w-full overflow-y-auto h-full">
-            <div className="chatroom flex flex-col items-center justify-center w-full">
-              <MessageBubble message="Hello world" sender="Me" />
-              <MessageBubble message="Hello world" sender={null} />
+            <div className="flex flex-col items-center justify-center w-full">
+              <MessagesList />
               <MessageInput />
             </div>
           </div>
