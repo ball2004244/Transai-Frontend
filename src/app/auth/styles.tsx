9ff063117a2fe68be6043a23bccf258db75c6d28
Tam Nguyen <nguyenminhtam7124@gmail.com>
2023-10-13 20:42:51 -0400

    Improve UI

--- src/app/auth/styles.tsx
+++ src/app/auth/styles.tsx
@@ -1,4 +1,4 @@
 export const inputFieldClass = "input-field text-sm md:text-lg text-gray-800 bg-gray-200 rounded-lg p-2 m-2 w-full md:w-3/4 outline-none";
 export const authButtonClass = "auth-button text-white font-bold text-lg bg-gray-700 rounded-lg p-2 m-2 w-full md:w-3/4";
 export const authTitleClass = "auth-title text-2xl text-center font-bold text-gray-800"
-export const authUIClass = "auth-content flex flex-col items-center justify-center w-[90vw] md:w-[40vw] min-h-[45vh] max-h-[70vh] bg-white rounded-lg p-10 m-2"
+export const authUIClass = "auth-content flex flex-col items-center justify-center w-[90vw] md:w-[40vw] min-h-[45vh] max-h-[80vh] bg-white rounded-lg p-4 m-2"
